8e54fd18dcbadbc5f70d4ab712229ddb15a46e36
eslam-ahmed43 <eslamelokpy35@gmail.com>
2026-06-14 11:46:50 +0300

    update google oauth redirect

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -31,7 +31,6 @@ export default function Home() {
     }, [])
 
 
-
     async function handleGoogleLogin() {
         setGoogleLoading(true)
         const { error } = await supabase.auth.signInWithOAuth({
